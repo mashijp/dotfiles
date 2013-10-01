d21ea696a5a628bc4e7a07435adc928e7f05ce4f
mashijp <mashijp@gmail.com>
2013-10-01 09:45:12 +0900

    gitの設定できるようにした

---
+++ setup_git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config --global color.ui auto
+
+echo '以下のようにGitの設定が必要です(自動では行われません)'
+echo 'git config --global user.name "mashijp"'
+echo 'git config --global user.email "EMAIL"'
+echo ''
