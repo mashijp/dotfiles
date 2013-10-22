e16d7b5db1e249037060741fb6bf3c7f5297fdf3
mashijp <mashijp@gmail.com>
2013-10-22 10:18:45 +0900

    gitignore global設定

--- setup_git.sh
+++ setup_git.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 git config --global color.ui auto
+git config --global --add core.excludesfile "$HOME/.gitignore"
 
 echo '以下のようにGitの設定が必要です(自動では行われません)'
 echo 'git config --global user.name "mashijp"'
